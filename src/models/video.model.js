c02a75784be55a5dd742f5f8516ef4eaa9c3fe2d
Shubhamt45054 <shubhamt45054@gmail.com>
2024-10-12 16:40:46 +0530

    nice

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,6 +1,6 @@
 import mongoose, {Schema} from "mongoose";
 
-// aggeration pipeine
+// apginate karne ke ability 
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema(
