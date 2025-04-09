c59dc4e8e65aaabbe886ee6e2ee732a8533ffa06
project45054 <project45054@gmail.com>
2025-04-09 23:34:03 +0530

    last

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// require('dotenv').config({path: './env'})
 import connectDB from "./src/db/index.js";
 import {app} from './app.js'
 import dotenv from "dotenv"
@@ -6,16 +5,6 @@ dotenv.config({
     path: './.env'
 })
 
-// import 'dotenv/config'
-
-// listen for error also
-// app.on();
-// kisi event ke liye listen karo error 
-
-// ascryonous methods return the promise also
-
-// console.log(process.env.CLOUDINARY_API_KEY);
-// console.log(process.env.CLOUDINARY_API_SECRET);
 
 connectDB()
 .then(() => {
@@ -26,42 +15,3 @@ connectDB()
 .catch((err) => {
     console.log("MONGO db connection failed !!! ", err);
 })
-
-
-
-
-
-
-
-
-
-
-/*
-connecting data base on spot ...
-
-import express from "express"
-const app = express()
-
-// using iffy
-
-( async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-
-        // to check if there is any error in express app.
-        app.on("errror", (error) => {
-            console.log("ERRR: ", error);
-            throw error
-        })
-
-        app.listen(process.env.PORT, () => {
-            console.log(`App is listening on port ${process.env.PORT}`);
-        })
-
-    } catch (error) {
-        console.error("ERROR: ", error)
-        throw err
-    }
-})()
-
-*/
